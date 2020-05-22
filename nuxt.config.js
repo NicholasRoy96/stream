6224da6cd7fe11626bc874b5113d63108ebc22f1
Nicholas Roy <nicholas.roy12@hotmail.co.uk>
2020-05-22 13:25:07 +0100

    Config

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,9 +11,9 @@ export default {
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: "description",
-        name: "description",
-        content: process.env.npm_package_description || ""
+        hid: "stream",
+        name: "stream",
+        content: "The only place to stream movies"
       }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico?v=2" }]

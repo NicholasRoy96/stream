e9c41ae88121316e704f753cd59c229c68803268
Nicholas Roy <nicholas.roy12@hotmail.co.uk>
2020-05-22 12:29:35 +0100

    Config

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,6 @@ export default {
    ** https://github.com/nuxt-community/vuetify-module
    */
   vuetify: {
-    customVariables: ["~/assets/variables.scss"],
     theme: {
       dark: true,
       themes: {

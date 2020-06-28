aa2980cddb5e14cf89a5dce243937a47987e22be
Nicholas Roy <nicholas.roy12@hotmail.co.uk>
2020-06-28 22:56:29 +0100

    Adding networks/watch now to bottom bar

--- components/infoPages/BottomBar.vue
+++ components/infoPages/BottomBar.vue
@@ -1,11 +1,32 @@
 <template>
-  <v-row no-gutters justify="space-around" align="center" class="background-row">
-    <v-btn icon depressed @click="goBack">
-      <v-icon size="28">mdi-arrow-left</v-icon>
-    </v-btn>
-    <TrailerDialog :bottomBar="true" />
-    <AddWatchlistIcon :bottomBar="true"/>
-  </v-row>
+  <div class="bottom-bar-div">
+    <div v-if="storeMedia.networksInfo && storeMedia.networksInfo.length">
+      <v-card tile class="networks-card">
+        <v-container>
+          <v-row justify="center" align="center">
+            <div v-if="storeMedia.networksInfo[0].logos && storeMedia.networksInfo[0].logos[0].file_path" class="network-logo-div">
+              <img :src="`https://image.tmdb.org/t/p/w300${storeMedia.networksInfo[0].logos[0].file_path}`" class="network-logo" />
+            </div>
+            <div v-if="storeMedia.networksInfo[0].homepage" class="d-flex-col pl-4">
+              <h2 class="network-subtitle">Now Streaming</h2>
+              <a :href="storeMedia.networksInfo[0].homepage" target="_blank" class="network-link">
+                <h1 class="network-title">Watch Now</h1>
+              </a>
+            </div>
+          </v-row>
+        </v-container>
+      </v-card>
+    </div>
+
+    <v-row no-gutters justify="space-around" align="center" class="background-row">
+      <v-btn icon depressed @click="goBack">
+        <v-icon size="28">mdi-arrow-left</v-icon>
+      </v-btn>
+      <TrailerDialog :bottomBar="true" />
+      <AddWatchlistIcon :bottomBar="true"/>
+    </v-row>
+
+  </div>
 </template>
 
 <script>
@@ -20,7 +41,7 @@ export default {
     TrailerDialog
   },
   computed: {
-    storeMovie() {
+    storeMedia() {
       return this.$store.state.media.media
     }
   },
@@ -33,12 +54,50 @@ export default {
 </script>
 
 <style scoped>
+.bottom-bar-div {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  opacity: 0.98;
+  margin-bottom: -1px;
+}
 .background-row {
   background-color: black;
   color: white;
-  position: fixed;
-  height: 40px;
-  bottom: 0;
-  width: 100%;
+  height: 50px;
+}
+.networks-card {
+  margin-bottom: -1px;
+  background-color: #0d0d0c;
+}
+.network-logo-div {
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 8px;
+  padding: 9px 9px;
+}
+.network-logo {
+  height: 26px;
+  max-width: 155px;
+}
+.network-link {
+  color: white;
+  text-decoration: none;
+}
+.network-link:hover {
+  color: #f5c518;
+}
+.network-title {
+  font-size: 0.95em;
+  font-weight: 600;
+  margin-top: -5px;
+}
+.network-subtitle {
+  font-size: 0.9em;
+  font-weight: 400;
+  opacity: 0.8;
+  letter-spacing: 0.03em;
 }
 </style>

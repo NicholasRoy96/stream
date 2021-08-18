c5eecafd032b89d75fc76c96af01f28445cb9ec0
Nick Roy <nicholas.roy12@hotmail.co.uk>
2021-08-18 19:37:34 +0100

    test git config commit

--- middleware/closeSearch.js
+++ middleware/closeSearch.js
@@ -1,7 +1,7 @@
 export default function ({ store }) {
     store.dispatch('closeSearchBar');
     if (process.client) {
-        // hacky fix to reset search icon color (Vuetify won't let me ...)
+        // hacky fix to reset search icon color (Vuetify won't let me ..)
         const searchIcon = document.getElementsByClassName('v-icon notranslate mdi mdi-magnify theme--dark')
         if (searchIcon) {
             searchIcon[0].style.color = ''
